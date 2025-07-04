d41ed1bd7091853c6adcbe92c1239ad1c694847a
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-04 08:03:19 +0200

    Put js back in

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import * as qstr from './qtools/qstr.js';
-
 const appElem = document.querySelector('#app');
-
 if (appElem) {
-	appElem.innerHTML = `
+    appElem.innerHTML = `
 	<h1>Info Site</h1>	
 	<p>${qstr.sentencize("welcome to this site")}</p>`;
 }
---
+++ js/qtools/qstr.js
@@ -0,0 +1,5 @@
+export const sentencize = (str) => {
+    if (!str)
+        return str;
+    return str.charAt(0).toUpperCase() + str.slice(1) + '.';
+};
