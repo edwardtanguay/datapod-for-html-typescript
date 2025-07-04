8ab0c5d8c34b27075567fd77fdaf4ee27ddaa705
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-04 07:48:34 +0200

    Delete js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import * as qstr from './qtools/qstr.js';
-const appElem = document.querySelector('#app');
-if (appElem) {
-    appElem.innerHTML = `
-	<h1>Info Site</h1>	
-	<p>${qstr.capitalizeFirstCharacter("welcome to this site")}</p>`;
-}
--- js/qtools/qstr.js
+++
@@ -1,5 +0,0 @@
-export const capitalizeFirstCharacter = (str) => {
-    if (!str)
-        return str;
-    return str.charAt(0).toUpperCase() + str.slice(1);
-};
