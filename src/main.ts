6aa9dce652d0abb7fef7b738aaf8bc3617f21b04
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-04 07:52:55 +0200

    Change title

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ const appElem = document.querySelector('#app');
 
 if (appElem) {
 	appElem.innerHTML = `
-	<h1>Info Site</h1>	
+	<h1>The Info Site</h1>	
 	<p>${qstr.sentencize("welcome to this site")}</p>`;
 }
